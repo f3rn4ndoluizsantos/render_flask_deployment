2e4891b981f258539ded10dbc198b5b9f2085dd8
Fernando Luiz <f3rn4ndoluizsantos@gmail.com>
2024-11-10 09:16:09 -0300

    ajuste app.py

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
+address = [
+    {
+        "id": 1,
+        "name": "John Doe",
+        "address": "123 Main St",
+        "city": "Anytown",
+        "state": "CA",
+    },
+    {
+        "id": 2,
+        "name": "Jane Doe",
+        "address": "456 Main St",
+        "city": "Anytown",
+        "state": "CA",
+    },
+]
+
+
 @app.route("/", methods=["GET"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return jsonify({"data": address}), 200
 
 
 if __name__ == "__main__":
